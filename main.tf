68d0f705e212437e03c96fb49abac8d77dc935ba
Mayank  Chaudhary <mankuchaudhary79@gmail.com>
2026-04-29 18:11:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
+resource "google_storage_bucket" "my-bucket" {
+  name          = "gh-action-demo-001"
   location      = "US"
   force_destroy = true
+  project="tt-dev
 
   lifecycle_rule {
     condition {
